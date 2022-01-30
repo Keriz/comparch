3837f4d5f8a4394e0e45fd0dce0bc17004d31d96
Keriz <guillaume.thivolet@free.fr>
2022-01-30 15:59:06 +0100

    Add MRU, RR and FIFO policies

--- lab1/src/cache.c
+++ lab1/src/cache.c
@@ -8,8 +8,9 @@
 #define INCORRECT_PARAMETERS 2
 #define INVALID_POINTER      3
 
-//extern
+// extern
 Cache *instruction_cache, *data_cache;
+uint32_t fifo_cntr = 0;
 
 static inline uint32_t c_log2(uint32_t v) {
 	return 31 - __builtin_clzl(v);
@@ -19,6 +20,15 @@ size_t least_recently_used_policy(Cache *c, const uint16_t set) {
 	if (!c) exit(INVALID_POINTER);
 	for (size_t i = 0; i < c->associativity; i++) {
 		if (c->sets[set].ways[i].recency == 0) {
+			return i;
+		}
+	}
+}
+
+size_t most_recently_used_policy(Cache *c, const uint16_t set) {
+	if (!c) exit(INVALID_POINTER);
+	for (size_t i = 0; i < c->associativity; i++) {
+		if (c->sets[set].ways[i].recency == 3) {
 			return i;
 		}
 	}
@@ -51,9 +61,9 @@ Cache *cache_init(uint32_t cache_size, uint32_t block_size, uint8_t associativit
 	c->sets = malloc(sizeof(Cache_set) * c->nb_sets);
 	if (!c->sets) exit(INVALID_ALLOCATION);
 
-	//FULLY ASSOCIATIVE?
+	// FULLY ASSOCIATIVE?
 	if (!associativity)
-		associativity != associativity; //set to max n-ways
+		associativity != associativity; // set to max n-ways
 
 	c->associativity = associativity;
 
@@ -64,7 +74,7 @@ Cache *cache_init(uint32_t cache_size, uint32_t block_size, uint8_t associativit
 		/* c->sets[i].ways->data = malloc(sizeof(uint8_t) * c->block_size);
 		if (!c->sets[i].ways->data) exit(INVALID_ALLOCATION); */
 
-		//set all cache tags to invalid
+		// set all cache tags to invalid
 		for (size_t j = 0; j < c->associativity; j++) {
 			c->sets[i].ways[j].v_flag  = 0;
 			c->sets[i].ways[j].tag     = 0;
@@ -85,24 +95,38 @@ Cache_response cache_access(Cache *c, uint32_t addr) {
 	uint16_t way_index = 0;
 
 	for (size_t i = 0; i < c->associativity; i++) {
-		//already in cache, nothing to do
+		// already in cache, nothing to do
 		way_index = i;
 		if (!c->sets[set].ways[i].v_flag) {
 			c->sets[set].ways[i].v_flag = 1;
 			c->sets[set].ways[i].tag    = tag;
+			// mru, lru
 			update_access_recency(c, set, way_index);
+			// fifo
+			fifo_cntr = (fifo_cntr + 1) % c->associativity;
 			return miss;
 		} else if (c->sets[set].ways[i].tag == tag) {
 			update_access_recency(c, set, way_index);
 			return hit;
 		}
 	}
-	//if we're here, that's a miss!
-	//increment cycles by 51, if d flag, write back, otherwise just load new memory
+	// if we're here, that's a miss!
+
+	// increment cycles by 51, if d flag, write back, otherwise just load new memory
 	/* 	if (c->sets[set].ways[way_index].d_flag) {
 	} */
-	size_t lru                 = least_recently_used_policy(c, set); //pick the LRU way in the set, where our new block will be
+
+	// lru, mru
+	size_t lru                 = least_recently_used_policy(c, set); // pick the LRU way in the set, where our new block will be
+	size_t mru                 = most_recently_used_policy(c, set);
 	c->sets[set].ways[lru].tag = tag;
+
+	// fifo
+	/* c->sets[set].ways[fifo_cntr].tag = tag;
+	fifo_cntr                        = (fifo_cntr + 1) % c->associativity; */
+
+	// random policy
+	// c->sets[set].ways[rand() % c->associativity].tag = tag;
 	update_access_recency(c, set, lru);
 	return miss;
 }
